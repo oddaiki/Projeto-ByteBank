8543670374da48f39208281d7a591c0f5d20b348
BuG4D0vj <viniciusj.ferreira12@gmail.com>
2023-02-14 00:02:24 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,71 @@
+﻿using Projeto_ByteBank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projeto_ByteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public double saldo = 100;
+
+        public Cliente titular;
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+
+
+    }
+}
+
---
+++ Projeto ByteBank/Program.cs
@@ -0,0 +1,69 @@
+﻿using Projeto_ByteBank.Contas;
+using Projeto_ByteBank.Titular;
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-x";
+//contaDoAndre.saldo = 100;
+//Console.WriteLine("Saldo da conta do André = R$ " + contaDoAndre.saldo);
+
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-x";
+//contaDoAndre.saldo = 100;
+//Console.WriteLine("Saldo da conta do André = R$ " + contaDoAndre2.saldo);
+
+//Console.WriteLine(contaDoAndre =contaDoAndre2);
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numero_agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
+
+//Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+//contaDoAndre.Transferir(50, contaDaMaria);
+//Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro = new ContaCorrente();
+//contaDoPedro.titular = "Pedro Silva";
+//Console.WriteLine(contaDoPedro.titular);
+//Console.WriteLine(contaDoPedro.saldo);
+//Console.WriteLine(contaDoPedro.numero_agencia);
+//Console.WriteLine(contaDoPedro.conta);
+
+
+Cliente cliente = new Cliente();
+cliente.nome = "Andre Silva";
+cliente.cpf = "123456789";
+cliente.profissao = "Analista";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 15;
+conta.saldo = 100;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("CPF = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("Nº Conta  = " + conta.titular.conta);
+Console.WriteLine("Saldo = " + conta.titular.saldo);
+Console.WriteLine("Nº Agência = " + conta.titular.numero_agencia);
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = new Cliente();
+conta2.titular = new Cliente();
+conta2.titular.nome = "Jose Souza";
+conta2.titular.profissao = "Tester";
+conta2.titular.cpf = "92468770861";
+conta2.conta = "9999-X";
+conta2.numero_agencia = 18;
+conta2.saldo = 500;
+
+Console.WriteLine(conta2.titular.nome);
+
